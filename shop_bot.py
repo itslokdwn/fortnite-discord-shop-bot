27c5b6e47cd57aac72da5e2b9e83e5484c5f1607
Ricky Terrero <120297572+itslokdwn@users.noreply.github.com>
2025-04-12 02:17:18 -0600

    Update shop_bot.py

--- shop_bot.py
+++ shop_bot.py
@@ -9,7 +9,7 @@ webhook_url = os.environ.get("DISCORD_WEBHOOK")
 api_key = os.environ.get("FORTNITE_API_KEY")
 
 # Correct endpoint for shop data.  Using the "br" endpoint is more reliable.
-api_url = "https://fortnite-api.com/v2/cosmetics/new"
+api_url = "https://fortnite-api.com/v2/cosmetics/br"
 
 def send_shop_items():
     """
